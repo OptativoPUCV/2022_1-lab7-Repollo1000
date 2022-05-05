5ae6a630987f840a8ef1cf8b7682a38e6e5115e6
Repollo1000 <>
2022-05-05 23:52:18 +0000

    new try

--- heap.c
+++ heap.c
@@ -89,7 +89,7 @@ void heap_pop(Heap* pq){
     //buscar mayor
     if(pq->heapArray[i].priority < pq->heapArray[hijo1].priority)
     {
-      posmay = i;
+      posmay = i + 1;
       auxPrio = pq->heapArray[0].priority;
       auxData = pq->heapArray[0].data;
       pq->heapArray[0].priority = pq->heapArray[posmay].priority;
