d4e9e43e9832629fc893d9bdba693912b052a3b1
Repollo1000 <>
2022-05-05 13:50:44 +0000

    new try

--- heap.c
+++ heap.c
@@ -51,8 +51,8 @@ void heap_push(Heap* pq, void* data, int priority){
       pq->heapArray[posActual].priority = auxPrio;
       pq->heapArray[posActual].data = auxData;
       pq->capac = pq->capac*2+1;
-      pq->heapArray = realloc(pq->heapArray, sizeof(pq->capac));
-      //pq->heapArray = (heapElem*)realloc(pq->heapArray, sizeof(pq->capac));
+      
+      pq->heapArray = (heapElem*)realloc(pq->heapArray, pq->capac * sizeof(int));
       
     }
   }
