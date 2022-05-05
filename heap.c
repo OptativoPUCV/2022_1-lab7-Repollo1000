e95c4ac73236757404f42438cdaede18d419582d
Repollo1000 <>
2022-05-05 15:40:06 +0000

    new try

--- heap.c
+++ heap.c
@@ -60,7 +60,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
-  int i = 0;
+  //int i = 0;
   int talla = pq->size;
   //int posActual = i - 1;
   //int padre = (posActual - 1)/2; 
