a9d4d2e5f83e0f1596e0d8226218395b34253df7
Repollo1000 <>
2022-05-05 15:53:49 +0000

    new try

--- heap.c
+++ heap.c
@@ -78,6 +78,7 @@ void heap_pop(Heap* pq){
 
   for(int i = 0 ; i < pq->size ; i++)
   {
+    i = i + 1;
     int hijos = (2*i + 1);
     if(pq->heapArray[i].priority > pq->heapArray[hijos].priority)
     {
